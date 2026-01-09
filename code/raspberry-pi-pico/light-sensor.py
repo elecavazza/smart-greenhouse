b017af7817a258413479163af7a597b3d2bd81ac
elecavazza <elecavazza02@gmail.com>
2026-01-09 15:21:18 +0000

    feat: add light sensor code

---
+++ code/raspberry-pi-pico/light-sensor.py
@@ -0,0 +1,8 @@
+from machine import Pin  
+import time
+ldr = machine.ADC(27)  # Initialize an ADC object for pin 27
+
+while True:
+    ldr_value = ldr.read_u16()  # Read the LDR value and convert it to a 16-bit unsigned integer
+    print("LDR Value:", ldr_value)  # Print the LDR value to the console
+    time.sleep(2)
