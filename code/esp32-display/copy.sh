03bce3bb89bc633cf445fef32346c2980ccc830b
elecavazza <elecavazza02@gmail.com>
2026-01-11 14:55:31 +0000

    chore: generalize setup and move to own folder

--- code/esp32-display/copy.sh
+++
@@ -1,2 +0,0 @@
-cp ./User_Setup.h /Users/elenacavazza/Documents/Arduino/libraries/TFT_eSPI/User_Setup.h
-cp ./lv_conf.h /Users/elenacavazza/Documents/Arduino/libraries/lv_conf.h
---
+++ code/esp32-display/setup/copy.sh
@@ -0,0 +1,2 @@
+cp ./User_Setup.h ~/Documents/Arduino/libraries/TFT_eSPI/User_Setup.h
+cp ./lv_conf.h ~/Documents/Arduino/libraries/lv_conf.h
