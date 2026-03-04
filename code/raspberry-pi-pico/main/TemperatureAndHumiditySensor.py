d00f4dab341ff503b6544696a9226518922690e1
elecavazza <elecavazza02@gmail.com>
2026-03-04 23:45:08 +0000

    feat: switch from AHT30 to AHT21   - AHT21 is more reliable and is on the same board as cO2 sensor

--- code/raspberry-pi-pico/main/TemperatureAndHumiditySensor.py
+++ code/raspberry-pi-pico/main/TemperatureAndHumiditySensor.py
@@ -1,18 +1,48 @@
 from machine import Pin, I2C
 import time
 
-address = 0x38
+ADDRESS = 0x38
+
 class TemperatureAndHumiditySensor:
+    """
+    Driver for AHT21 temperature and humidity sensor.
+    
+    Wiring (using VIN, GND, SCL, SDA pins):
+    - VIN -> 3.3V or 5V
+    - GND -> GND
+    - SCL -> I2C clock pin
+    - SDA -> I2C data pin
+    """
+    
     def __init__(self, serialClockLinePinNumber, serialDataLinePinNumber):
         self.i2c = I2C(0, scl=Pin(serialClockLinePinNumber), sda=Pin(serialDataLinePinNumber))
         time.sleep_ms(40)
+        self._initialize()
+
+    def _initialize(self):
+        """Initialize and calibrate the AHT21 sensor."""
+        # Send calibration command
+        self.i2c.writeto(ADDRESS, bytes([0xBE, 0x08, 0x00]))
+        time.sleep_ms(100)
+        
+        # Check calibration status
+        self.i2c.writeto(ADDRESS, bytes([0x71]))
+        status = self.i2c.readfrom(ADDRESS, 1)
+        
+        if not (status[0] & 0x68) == 0x08:
+            raise Exception("AHT21 initialization failed!")
 
     def read(self):
+        """Read temperature and humidity from the sensor.
+        
+        Returns:
+            tuple: (temperature in Celsius, humidity percentage)
+        """
         # Trigger measurement
-        self.i2c.writeto(address, b'\xAC\x33\x00')
+        self.i2c.writeto(ADDRESS, b'\xAC\x33\x00')
         time.sleep_ms(80)
 
-        data = self.i2c.readfrom(address, 6)
+        data = self.i2c.readfrom(ADDRESS, 6)
 
         raw_humidity = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
         raw_temperature = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
